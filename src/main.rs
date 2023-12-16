13b1598cf7339c94abc6704af93f2b2e1b950d05
LordWatson <alexander.watson.work@gmail.com>
2023-12-16 12:27:19 +0000

    user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// import user input package
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    
+    println!("Feed me your first input");
+    
+    // create input
+    io::stdin()
+    // & symbol to create reference
+    // this gives us a modifiable reference to the input variable
+    // returns result object
+    .read_line(&mut input)
+    // .expect checks if .read_line gives back a valid value
+    .expect("Failed to read line");
+    
+    println!("{}", input);
 }
